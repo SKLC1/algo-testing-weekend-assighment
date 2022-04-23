ccee67eb41f40876d1a399633b96d2a1888babde
SKLC1 <segevklein2000@gmail.com>
2022-04-24 00:00:03 +0300

    addded longestCommon

--- Solutions/logestCommonPrefix/longestCommonPrefix.easy.js
+++ Solutions/logestCommonPrefix/longestCommonPrefix.easy.js
@@ -17,13 +17,22 @@
  */
 
 const longestCommonPrefix = function (strs) {
-  let two = strs[0].slice(0, 2);
-  for (let i = 1; i < strs.length; i++) {
-    if (two !== strs[i].slice(0, 2)) {
-      return "";
+  let match = [];
+  let match2 = [];
+  let word1 = strs[0].toString().split('')
+  let word2 = strs[1].toString().split('')
+  let word3 = strs[2].toString().split('')
+  for (let i = 0; i < word1.length; i++) {
+    if (word1[i] === word2[i]) {
+      match.push(word1[i])
     }
   }
-  return two;
+  for (let i = 0; i < match.length; i++) {
+    if (word2[i] === word3[i]) {
+      match2.push(word1[i])
+    }
+  }
+  return match2.join('')
 };
 console.log(longestCommonPrefix(["flower","flow","flight"]));
 console.log(longestCommonPrefix(["dog","racecar","car"]));
